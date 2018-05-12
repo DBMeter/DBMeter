7d18e228b5716ac32314c6dec5a8422e5b2fd77e
Anton Okolelov <okoanton@yandex.ru>
2018-05-12 17:49:16 +0300

    Config reader (#8)
    * Config reader
    
    * rust fmt

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,19 @@
 extern crate chrono;
 extern crate postgres;
 
-use std::{thread, env};
-use std::time::Duration;
-use postgres::{Connection, TlsMode};
-use chrono::Local;
+mod config;
+mod postgres_monitoring;
 
+use chrono::Local;
+use config::Config;
+use postgres_monitoring::start_monitoring;
 
 fn main() {
     println!("Start daemon {}", Local::now());
-    // for example,  "postgresql://anton:12345@localhost:5432/l09";
-    let postgres_url = env::var("DBMETER_DB_DSN").unwrap();
-    println!("postgres url: {}", postgres_url);
-    start_monitoring(postgres_url);
-}
 
-fn start_monitoring(postgres_url: String) {
-    let result = Connection::connect(postgres_url, TlsMode::None);
-    match result {
-        Ok(_result) => println!("ok"),
-        Err(_error) => println!("error: {}", _error)
-    }
+    let config = Config::new();
 
-    loop {
-        thread::sleep(Duration::from_secs(1));
-        println!("next ");
+    if let Some(dsn) = config.get_postgres() {
+        start_monitoring(dsn);
     }
 }
